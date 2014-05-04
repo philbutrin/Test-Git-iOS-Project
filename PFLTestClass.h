a6d6a0fedf4f02704357c40c15f5095e0fc0a468
philbutrin <dividedby@gmail.com>
2014-05-04 12:36:29 -0400

    added test class

---
+++ PFLTestClass.h
@@ -0,0 +1,13 @@
+//
+//  PFLTestClass.h
+//  Git Test
+//
+//  Created by Philip Locke on 5/4/14.
+//  Copyright (c) 2014 Phil Locke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PFLTestClass : NSObject
+
+@end
---
+++ PFLTestClass.m
@@ -0,0 +1,13 @@
+//
+//  PFLTestClass.m
+//  Git Test
+//
+//  Created by Philip Locke on 5/4/14.
+//  Copyright (c) 2014 Phil Locke. All rights reserved.
+//
+
+#import "PFLTestClass.h"
+
+@implementation PFLTestClass
+
+@end
